a365548f39f9d440dcdae81506c8d5c4483bfc43
GianRathgeb <gian.rathgeb@bbz-sh.ch>
2021-01-29 15:00:36 +0100

    First tests to scan a singel port

---
+++ scanSinglePort.py
@@ -0,0 +1,29 @@
+import socket
+import sys
+strHostname = 'localhost'
+
+
+target = socket.gethostbyname(strHostname)
+print(target)
+
+try: 
+    # will scan ports between 1 to 65,535 
+    for port in range(50): 
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        #socket.setdefaulttimeout(1) 
+          
+        # returns an error indicator 
+        result = s.connect_ex((target,port)) 
+        if result == 0: 
+            print("Port {} is open".format(port)) 
+        s.close() 
+          
+except KeyboardInterrupt: 
+    print("\n Exitting Program !!!!") 
+    sys.exit() 
+except socket.gaierror: 
+    print("\n Hostname Could Not Be Resolved !!!!") 
+    sys.exit() 
+except socket.error: 
+    print("\n Server not responding !!!!") 
+    sys.exit() 
