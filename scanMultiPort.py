49af58185fb10651b10d6c4b7dac994d8a321137
gianrathgeb <gian.rathgeb@bbz-sh.ch>
2021-02-03 21:44:31 +0100

    added file for multi port, but not yet edited much

---
+++ scanMultiPort.py
@@ -0,0 +1,30 @@
+import socket
+import sys
+
+def fnScanPort(hostname, portRange):
+    if not isinstance(portRange, int):
+        portRange = int(portRange)
+    target = socket.gethostbyname(hostname)
+
+    try: 
+
+        # will scan ports between 1 to 65,535 
+        for port in range(79, 81): 
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+            print(f'Checking port {port}')
+            
+            # returns an error indicator 
+            result = s.connect_ex((target,port)) 
+            if result == 0: 
+                print("Port {} is open".format(port)) 
+            s.close()    
+
+    except KeyboardInterrupt: 
+        print("\n Exitting Program !!!!") 
+        sys.exit() 
+    except socket.gaierror: 
+        print("\n Hostname Could Not Be Resolved !!!!") 
+        sys.exit() 
+    except socket.error: 
+        print("\n Server not responding !!!!") 
+        sys.exit() 
--- scanSinglePort.py
+++ scanSinglePort.py
@@ -7,21 +7,6 @@ def fnScanPort(hostname, port):
     target = socket.gethostbyname(hostname)
 
     try: 
-        #! Code for multi port scan
-        ''' code to check multiple ports
-        # will scan ports between 1 to 65,535 
-        for port in range(79, 81): 
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-            print(f'Checking port {port}')
-            
-            # returns an error indicator 
-            result = s.connect_ex((target,port)) 
-            if result == 0: 
-                print("Port {} is open".format(port)) 
-            s.close() 
-        '''    
-
-
         #! Code for single port scan
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         print(f'Checking port {port}')
